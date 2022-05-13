1b22e3e16efc00c03e433c9816bc2d1a7ee907b3
Boston Smith <boston_smith@outlook.com>
2022-05-12 22:01:58 -0500

    finalized bottomnav

--- app/components/BottomNavigation.tsx
+++ app/components/BottomNavigation.tsx
@@ -1,6 +1,9 @@
 import { navItems } from '~/shared/List'
+interface Props {
+  children: React.ReactNode
+}
 
-const BottomNavContainer = ({ children }: Props) => {
+const NavContainer = ({ children }: Props) => {
   return (
     <div className='bg-gray-50 fixed bottom-0 w-full shadow-inner px-8 py-2 flex flex-row justify-between md:hidden'>
       {children}
@@ -8,37 +11,19 @@ const BottomNavContainer = ({ children }: Props) => {
   )
 }
 
-const BottomNavItemContainer = ({ children }) => {
+const BottomNavigation = () => {
   return (
-    <>
+    <NavContainer>
       {navItems.map(navItem => (
         <div
           key={navItem.name}
           className='flex flex-col items-center justify-center space-y-1'
         >
-          <BottomNavItem navItem={navItem} />
+          <navItem.icon className='h-6 w-6 text-gray-600' />
+          <div className='text-xs text-gray-900'>{navItem.name}</div>
         </div>
       ))}
-    </>
-  )
-}
-
-const BottomNavItem = ({ navItem }) => {
-  return (
-    <>
-      <navItem.icon className='h-6 w-6 text-gray-600' />
-      <div className='text-xs text-gray-900'>{navItem.name}</div>
-    </>
-  )
-}
-
-const BottomNavigation = () => {
-  return (
-    <BottomNavContainer>
-      <BottomNavItemContainer>
-        <BottomNavItem />
-      </BottomNavItemContainer>
-    </BottomNavContainer>
+    </NavContainer>
   )
 }
 
