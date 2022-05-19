b0f08a3f59dc93b99ae9a84947d1b3e2d6751348
Boston Smith <boston_smith@outlook.com>
2022-05-18 19:08:43 -0500

    Refactored hero section to accept props

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
+export type HeroSectionProps = {
+  title: string | React.ReactNode
+  subtitle?: string | React.ReactNode
+  description?: string | React.ReactNode
+}
 
-const Hero = () => {
+const Hero = ({ title, subtitle, description }: HeroSectionProps) => {
   return (
     <section id='home' className='px-5vw'>
       <div className='container mx-auto'>
         <div className='relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8'>
           <h1 className='text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-7xl'>
             <span className='block bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply text-transparent bg-clip-text'>
-              Boston Smith
+              {title}
             </span>
-            <span className='block text-black whitespace-pre'>
-              Full Stack Web Developer
-            </span>
+            <span className='block text-black whitespace-pre'>{subtitle}</span>
           </h1>
           <p className='mt-6 max-w-lg mx-auto text-center text-xl text-gray-500 sm:max-w-3xl'>
-            I'm a full stack web developer who has a passion for solving
-            business problems with code. Let's start scrolling and learn more
-            about me.
+            {description}
           </p>
         </div>
       </div>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,13 @@ import Hero from '~/components/sections/Hero'
 export default function Index() {
   return (
     <main>
-      <Hero />
+      <Hero
+        title='Boston Smith'
+        subtitle='Full Stack Web Developer'
+        description="I'm a full stack web developer who has a passion for solving
+        business problems with code. Let's start scrolling and learn more
+        about me."
+      />
     </main>
   )
 }
