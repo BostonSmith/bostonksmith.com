b785f0587a709e3314cd7a9e5f8215d1f05845fa
Boston Smith <boston_smith@outlook.com>
2022-05-21 10:47:47 -0500

    updated hero-section

--- app/components/sections/Hero-Section.tsx
+++ app/components/sections/Hero-Section.tsx
@@ -6,9 +6,8 @@ interface HeroSectionProps {
 
 const HeroSection = ({ title, subtitle, description }: HeroSectionProps) => {
   return (
-    <section id='home' className='px-5vw'>
+    <section id='home' className='px-5vw py-16 sm:py-24 lg:py-32'>
       <div className='container mx-auto'>
-        <div className='relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8'>
           <h1 className='text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-7xl'>
             <span className='block bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply text-transparent bg-clip-text'>
               {title}
@@ -18,7 +17,6 @@ const HeroSection = ({ title, subtitle, description }: HeroSectionProps) => {
           {description && <p className='mt-6 max-w-lg mx-auto text-center text-xl text-gray-500 sm:max-w-3xl'>
             {description}
           </p>}
-        </div>
       </div>
     </section>
   )
